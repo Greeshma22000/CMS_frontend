78faa618d9c9c3b5d5fb555c898b8703c0409920
Greeshma Madala <83536924+Greeshma22000@users.noreply.github.com>
2025-12-14 11:03:57 +0530

    .

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:5001/api"
+    baseURL: "http://localhost:5001/api",
+    headers: {
+        "Content-Type": "application/json",
+    },
 });
 
 API.interceptors.request.use((req) => {
