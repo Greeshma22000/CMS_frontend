951aa940ffc236bb9e7e37c5f32610f6a077c65f
Greeshma Madala <83536924+Greeshma22000@users.noreply.github.com>
2025-12-14 14:05:12 +0530

    .

---
+++ src/pages/Blogs.jsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+import API from "../api/api";
+
+const Blogs = () => {
+  const [blogs, setBlogs] = useState([]);
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  const fetchBlogs = async () => {
+    const res = await API.get("/blogs");
+    setBlogs(res.data);
+  };
+
+  const addBlog = async () => {
+    await API.post("/blogs", { title, content });
+    fetchBlogs();
+  };
+
+  const deleteBlog = async (id) => {
+    await API.delete(`/blogs/${id}`);
+    fetchBlogs();
+  };
+
+  useEffect(() => {
+    fetchBlogs();
+  }, []);
+
+  return (
+    <div>
+      <h2>Blogs</h2>
+
+      <input placeholder="Title" onChange={e => setTitle(e.target.value)} />
+      <textarea placeholder="Content" onChange={e => setContent(e.target.value)} />
+      <button onClick={addBlog}>Add</button>
+
+      {blogs.map(blog => (
+        <div key={blog._id}>
+          <h4>{blog.title}</h4>
+          <button onClick={() => deleteBlog(blog._id)}>Delete</button>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Blogs;
