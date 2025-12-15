44ad3d13438553f8b034dc41694cb8c1c0234ea0
Greeshma Madala <83536924+Greeshma22000@users.noreply.github.com>
2025-12-15 17:58:48 +0530

    .

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,16 +2,16 @@ import { Link } from "react-router-dom";
 
 const Sidebar = () => {
   return (
-    <div className="w-64 h-full fixed bg-white shadow-2xl rounded-md">
-      <nav className="flex flex-col p-8 space-y-8 text-xl font-semibold">
-        <Link to="/" className="shadow p-2">Dashboard</Link>
-        <Link to="/about" className="shadow p-2">About</Link>
-        <Link to="/skills" className="shadow p-2">Skills</Link>
-        <Link to="/projects" className="shadow p-2">Projects</Link>
-        <Link to="/blogs" className="shadow p-2">Blogs</Link>
-        <Link to="/testimonials" className="shadow p-2">Testimonials</Link>
-        <Link to="/experience" className="shadow p-2">Experience</Link>
-        <Link to="/services" className="shadow p-2">Services</Link>
+    <div className="w-64 h-full fixed bg-white shadow-2xl rounded-2xl">
+      <nav className="flex flex-col p-8 space-y-9 text-xl font-semibold italic">
+        <Link to="/" className="shadow-xl p-2 rounded-2xl border-l-8 border-blue-600 shadow-blue-400 border">Dashboard</Link>
+        <Link to="/about" className="hover:shadow-xl hover:p-2 hover:rounded-2xl hover:scale-3d hover:transition-all hover:duration-300 hover:bg-blue-300 hover:text-white">About</Link>
+        <Link to="/skills" className="hover:shadow-xl hover:p-2 hover:rounded-2xl hover:scale-3d hover:transition-all hover:duration-300 hover:bg-blue-300 hover:text-white">Skills</Link>
+        <Link to="/projects" className="hover:shadow-xl hover:p-2 hover:rounded-2xl hover:scale-3d hover:transition-all hover:duration-300 hover:bg-blue-300 hover:text-white">Projects</Link>
+        <Link to="/blogs" className="hover:shadow-xl hover:p-2 hover:rounded-2xl hover:scale-3d hover:transition-all hover:duration-300 hover:bg-blue-300 hover:text-white">Blogs</Link>
+        <Link to="/testimonials" className="hover:shadow-xl hover:p-2 hover:rounded-2xl hover:scale-3d hover:transition-all hover:duration-300 hover:bg-blue-300 hover:text-white">Testimonials</Link>
+        <Link to="/experience" className="hover:shadow-xl hover:p-2 hover:rounded-2xl hover:scale-3d hover:transition-all hover:duration-300 hover:bg-blue-300 hover:text-white">Experience</Link>
+        <Link to="/services" className="hover:shadow-xl hover:p-2 hover:rounded-2xl hover:scale-3d hover:transition-all hover:duration-300 hover:bg-blue-300 hover:text-white">Services</Link>
       </nav>
     </div>
   )
