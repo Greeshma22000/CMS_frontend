a23c0529e041f55ba01d88082ad74e109a8ecd74
Greeshma Madala <83536924+Greeshma22000@users.noreply.github.com>
2025-12-15 17:42:48 +0530

    .

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,15 +2,17 @@ import { Link } from "react-router-dom";
 
 const Sidebar = () => {
   return (
-    <div>
-        <Link to="/">Dashboard</Link>
-        <Link to="/about">About</Link>
-        <Link to="/skills">Skills</Link>
-        <Link to="/projects">Projects</Link>
-        <Link to="/blogs">Blogs</Link>
-        <Link to="/testimonials">Testimonials</Link>
-        <Link to="/experience">Experience</Link>
-        <Link to="/services">Services</Link>
+    <div className="w-64 h-full fixed bg-white shadow-2xl rounded-md">
+      <nav className="flex flex-col p-8 space-y-8 text-xl font-semibold">
+        <Link to="/" className="shadow p-2">Dashboard</Link>
+        <Link to="/about" className="shadow p-2">About</Link>
+        <Link to="/skills" className="shadow p-2">Skills</Link>
+        <Link to="/projects" className="shadow p-2">Projects</Link>
+        <Link to="/blogs" className="shadow p-2">Blogs</Link>
+        <Link to="/testimonials" className="shadow p-2">Testimonials</Link>
+        <Link to="/experience" className="shadow p-2">Experience</Link>
+        <Link to="/services" className="shadow p-2">Services</Link>
+      </nav>
     </div>
   )
 }
