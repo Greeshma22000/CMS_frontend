3ae3027103c392bfa94ee8993e2bb659b619452a
Greeshma Madala <83536924+Greeshma22000@users.noreply.github.com>
2025-12-15 18:17:50 +0530

    .

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,17 +1,30 @@
 import { Link } from "react-router-dom";
 
 const Sidebar = () => {
+
+  const SidebarLinks = [
+    // {path: "/", title:"Dashboard"},
+    {path: "/about", title:"About"},
+    {path: "/skills", title:"Skills"},
+    {path: "/projects", title:"Projects"},
+    {path: "/blogs", title:"Blogs"},
+    {path: "/testimonials", title:"Testimonials"},
+    {path: "/experience", title:"Experience"},
+    {path: "/service", title:"Service"},
+  ];
   return (
     <div className="w-64 h-full fixed bg-white shadow-2xl rounded-2xl">
-      <nav className="flex flex-col p-8 space-y-9 text-xl font-semibold italic">
-        <Link to="/" className="shadow-xl p-2 rounded-2xl border-l-8 border-blue-600 shadow-blue-400 border">Dashboard</Link>
-        <Link to="/about" className="hover:shadow-xl hover:p-2 hover:rounded-2xl hover:scale-3d hover:transition-all hover:duration-300 hover:bg-blue-300 hover:text-white">About</Link>
-        <Link to="/skills" className="hover:shadow-xl hover:p-2 hover:rounded-2xl hover:scale-3d hover:transition-all hover:duration-300 hover:bg-blue-300 hover:text-white">Skills</Link>
-        <Link to="/projects" className="hover:shadow-xl hover:p-2 hover:rounded-2xl hover:scale-3d hover:transition-all hover:duration-300 hover:bg-blue-300 hover:text-white">Projects</Link>
-        <Link to="/blogs" className="hover:shadow-xl hover:p-2 hover:rounded-2xl hover:scale-3d hover:transition-all hover:duration-300 hover:bg-blue-300 hover:text-white">Blogs</Link>
-        <Link to="/testimonials" className="hover:shadow-xl hover:p-2 hover:rounded-2xl hover:scale-3d hover:transition-all hover:duration-300 hover:bg-blue-300 hover:text-white">Testimonials</Link>
-        <Link to="/experience" className="hover:shadow-xl hover:p-2 hover:rounded-2xl hover:scale-3d hover:transition-all hover:duration-300 hover:bg-blue-300 hover:text-white">Experience</Link>
-        <Link to="/services" className="hover:shadow-xl hover:p-2 hover:rounded-2xl hover:scale-3d hover:transition-all hover:duration-300 hover:bg-blue-300 hover:text-white">Services</Link>
+      <nav className="flex flex-col p-8 space-y-8 text-xl font-semibold italic">
+        <Link to="/" className="shadow-xl p-2 rounded-2xl border-l-8 border-blue-600 shadow-blue-400/60 border">Dashboard</Link>
+        {SidebarLinks.map((link) => (
+          <Link 
+            key={link.path} 
+            to={link.path}
+            className="shadow-xl rounded-2xl p-2 shadow-blue-200 hover:shadow-xl hover:p-2 hover:rounded-2xl hover:scale-3d hover:transition-all hover:duration-300 hover:bg-blue-300 hover:text-white"
+          >
+            {link.title}
+          </Link>
+        ))}
       </nav>
     </div>
   )
